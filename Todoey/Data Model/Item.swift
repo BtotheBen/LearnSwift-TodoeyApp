a302f707877d293cc7a0c25c498502cd32991b38
BtotheBen <bbruchwald@gmail.com>
2023-10-01 15:59:07 +0200

    244 Done

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by BB on 01.10.23.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+}
